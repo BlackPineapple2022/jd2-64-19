6fdc44c1bc44f192e4cbbb361493c5257a719d44
Blackpineapple2022 <blackpineapple2022@gmail.com>
2019-12-24 14:19:58 +0300

    TripScanner added

--- travel-company/travel-company-web/src/main/java/by/academy/it/travelcompany/listener/TravelCompanyContextInitListener.java
+++ travel-company/travel-company-web/src/main/java/by/academy/it/travelcompany/listener/TravelCompanyContextInitListener.java
@@ -1,13 +1,18 @@
 package by.academy.it.travelcompany.listener;
 
+import by.academy.it.travelcompany.db.connection.pool.TcDataSource;
+import by.academy.it.travelcompany.db.migration.DbMigration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+import java.util.ResourceBundle;
 
-    @WebListener()
+
+@WebListener()
     public class TravelCompanyContextInitListener implements ServletContextListener{
 
         private static final Logger logger = LoggerFactory.getLogger(TravelCompanyContextInitListener.class);
@@ -15,6 +20,15 @@ import javax.servlet.annotation.WebListener;
         @Override
         public void contextInitialized(ServletContextEvent sce) {
             logger.info("Context initialized");
+            try {
+                ResourceBundle bundle = ResourceBundle.getBundle("mysql_hikari");
+                TcDataSource.configure(bundle);
+                DataSource dataSource = TcDataSource.getDataSource();
+                DbMigration.migrate(dataSource);
+            } catch (Exception e) {
+                logger.error("error", e);
+                throw new RuntimeException("Datasource initialisation error", e);
+            }
         }
 
         @Override
