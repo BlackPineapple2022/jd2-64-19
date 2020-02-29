7d1c09f68b8801dab1869066b59603933e00167a
Blackpineapple2022 <blackpineapple2022@gmail.com>
2020-02-29 14:58:55 +0300

    spring framework homework

---
+++ travel-company/common-spring/src/main/java/by/academy/it/travelcompany/beans/Person.java
@@ -0,0 +1,49 @@
+package by.academy.it.travelcompany.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private Long id;
+    private String name;
+    private String surname;
+
+    private PersonInfo personInfo;
+
+    private Department department;
+
+    private List<Meeting> meetings;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(surname, person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", personInfo=" + personInfo +
+                ", department=" + department +
+                '}';
+    }
+}
