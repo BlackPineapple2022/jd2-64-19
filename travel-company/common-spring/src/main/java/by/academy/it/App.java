460e1e0120bf878b909c179372a80eccbc0d9acb
Blackpineapple2022 <blackpineapple2022@gmail.com>
2020-02-29 10:34:43 +0300

    13 wizz api

--- travel-company/common-spring/src/main/java/by/academy/it/App.java
+++ travel-company/common-spring/src/main/java/by/academy/it/App.java
@@ -3,9 +3,16 @@ package by.academy.it;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class App {
     public static void main(String[] args) {
-
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(2);
+        integerList.add(1);
+        integerList.remove(new Integer(1));
+        System.out.println(integerList);
     }
 }
