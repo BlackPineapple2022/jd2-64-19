0029ebd9908398f8ec4b3127e7659a456e673a8f
Blackpineapple2022 <blackpineapple2022@gmail.com>
2020-02-29 11:47:41 +0300

    spring framework started configuration

--- travel-company/common-spring/src/main/java/by/academy/it/App.java
+++ travel-company/common-spring/src/main/java/by/academy/it/App.java
@@ -9,10 +9,6 @@ import java.util.List;
 @Slf4j
 public class App {
     public static void main(String[] args) {
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(2);
-        integerList.add(1);
-        integerList.remove(new Integer(1));
-        System.out.println(integerList);
+
     }
 }
